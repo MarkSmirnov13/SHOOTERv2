8508d7b040d9a476e1ddffd169cede6fed3a853a
Господин <marksmirnov13@gmail.com>
2016-06-06 15:53:56 +0300

    Done

--- app/src/main/java/com/example/user/shooter/MainActivity.java
+++ app/src/main/java/com/example/user/shooter/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.user.shooter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -13,6 +17,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Drawable pict = DrawableFactory.decodeResource(getResources(), R.drawable.bg);
+        ImageView bg = (ImageView) findViewById(R.id.imageView);
+        bg.setImageDrawable(getResources().getDrawable(R.drawable.bd_initial));
     }
 
     public void onStartGameButton(View v) {
